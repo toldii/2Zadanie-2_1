419848054ed06181d33283edd55ac97a53b1c31a
toldii1 <100698275+toldii@users.noreply.github.com>
2022-03-29 12:16:38 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+     Scanner scan = new Scanner(System.in);
+     System.out.println("wybierz rozmiar ");
+    int a= scan.nextInt();
+     //System.out.println("wybierz z czego ma byc zrobiona choinka");
+    //int b= scan.nextInt();
+    char c= '*';
+    for (int i = 1; i<=a; i++)
+        {
+            for(int j=0;j<a-i;j++);
+            for (int j = 0; j<i; j++)System.out.print(c);
+            System.out.println(" ");
+        }
+     
+  }
+}
